3cd739ae4664ba16d0f52c08e94d5c4ff0e9754e
Lautaro Perez Crespo <lautypc@hotmail.com>
2023-11-28 20:57:27 -0300

    fix

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -6,11 +6,6 @@ import { describe, expect, it } from 'vitest';
 describe('App', () => {
   it('should renders component', () => {
     const component = render(<App />)
-
-    const title = component.getByText("ESTO NO ESTA")
-
-    expect(title).toBe("asdasd")
-
-     /* expect(component).toBeDefined() */
+    expect(component).toBeDefined() 
   });
 });
