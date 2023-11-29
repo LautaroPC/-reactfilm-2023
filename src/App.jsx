f26ae72eea091e4a52860474b6cca1c4d87250d5
Lautaro Perez Crespo <lautypc@hotmail.com>
2023-11-29 08:14:04 -0300

    [cfg] - changes proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { environment } from "../environments/environments.qa"
+
 const App = () => {
   return (
     <div>
       <h1>APP DE PELIS</h1>
-      <h2>ESTO ES QA</h2>
+      <h2>{environment.title}</h2>
     </div>
   )
 }
